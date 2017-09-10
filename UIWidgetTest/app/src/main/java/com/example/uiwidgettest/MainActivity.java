505b1c7f2282101de57f20a119eeeaf7ca61aa2e
Slow_Time <1395881075@qq.com>
2017-09-10 20:21:24 +0800

    ProgressBar

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
 
     private ImageView imageView;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +25,11 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button)findViewById(R.id.button);
         editText = (EditText)findViewById(R.id.edit_text);
         imageView = (ImageView)findViewById(R.id.image_view);
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imageView.setImageResource(R.drawable.img_2);
+                progressBar.setProgress(progressBar.getProgress() + 10);
             }
         });
     }
