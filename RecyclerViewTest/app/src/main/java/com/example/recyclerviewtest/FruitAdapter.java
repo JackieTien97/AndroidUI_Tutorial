3d1fefd69cbfacebb54609f5ae98e9b05840b584
Slow_Time <1395881075@qq.com>
2017-09-11 20:22:16 +0800

    水平滚动的RecyclerView

---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.java
@@ -0,0 +1,53 @@
+package com.example.recyclerviewtest;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by slow_time on 2017/9/11.
+ */
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private List<Fruit> fruitList;
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        this.fruitList = fruitList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Fruit fruit = fruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return fruitList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            fruitImage = itemView.findViewById(R.id.fruit_image);
+            fruitName = itemView.findViewById(R.id.fruit_name);
+        }
+    }
+}
---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.recyclerviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+        FruitAdapter fruitAdapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(fruitAdapter);
+    }
+
+    private void initFruits() {
+        for (int i = 0; i < 20; i++) {
+            Fruit fruit = new Fruit("Apple", R.drawable.apple);
+            fruitList.add(fruit);
+        }
+    }
+}
